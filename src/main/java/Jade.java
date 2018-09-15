a5fecf1238b3a643141bd5fafc688b2fe8ebdcbc
Luiz <Luiz@Luiz-PC>
2018-09-15 18:26:43 -0300

    JADE FUNCIONANDO PORRA!!!! Gerador de voz ainda não :(

--- src/main/java/Jade.java
+++ src/main/java/Jade.java
@@ -1,3 +1,6 @@
+
+import jade.core.Agent;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,13 +11,19 @@
  *
  * @author Lidera Consultoria
  */
-public class Jade {
+public class Jade extends Agent{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Jade jade = new Jade();
+        jade.setup();
     }
     
+     protected void setup() 
+     { 
+          System.out.println(getLocalName()); 
+     }
 }
