1cba45bbae7e71430c67a7ce4dca44c8e125630f
Luiz <Luiz@Luiz-PC>
2018-09-16 12:25:44 -0300

    tudo ok!! Desenvolver o resto

--- src/main/java/Agentes/GeradorVoz.java
+++ src/main/java/Agentes/GeradorVoz.java
@@ -19,49 +19,49 @@ import java.io.OutputStream;
  *
  * @author Lidera Consultoria
  */
-public class GeradorVoz extends Agent{
-            protected void setup(){
+public class GeradorVoz extends Agent {
+
+    protected void setup() {
         System.out.println("Gerador de voz incializado");
-         addBehaviour (new CyclicBehaviour(this) {
+        addBehaviour(new CyclicBehaviour(this) {
             public void action() {
-                ACLMessage msg =receive() ;
-                if (msg!=null )
-            System.out.println(" - " + myAgent.getLocalName( )+"<- " + msg.getContent());
-            // interrompe este comportamento ate que chegue uma nova mensagem
-            
-            TextToSpeech textToSpeech = new TextToSpeech();
-        textToSpeech.setUsernameAndPassword("474a2e01-7aef-47b3-8dc7-7e6c3be4ee45", "idIWedWDbmfW");
-        try {
-            SynthesizeOptions synthesizeOptions
-                    = new SynthesizeOptions.Builder()
-                            .text(msg.getContent())
-                            .accept("audio/wav")
-                            .voice("pt-BR_IsabelaVoice")
-                            .build();
+                ACLMessage msg = receive();
+                if (msg != null) {
+                    System.out.println(" - " + myAgent.getLocalName() + "<- " + msg.getContent());
+                    // interrompe este comportamento ate que chegue uma nova mensagem
 
-            InputStream inputStream
-                    = textToSpeech.synthesize(synthesizeOptions).execute();
-            InputStream in = WaveUtils.reWriteWaveHeader(inputStream);
+                    TextToSpeech textToSpeech = new TextToSpeech();
+                    textToSpeech.setUsernameAndPassword("474a2e01-7aef-47b3-8dc7-7e6c3be4ee45", "idIWedWDbmfW");
+                    try {
+                        SynthesizeOptions synthesizeOptions
+                                = new SynthesizeOptions.Builder()
+                                .text(msg.getContent())
+                                .accept("audio/wav")
+                                .voice("pt-BR_IsabelaVoice")
+                                .build();
 
-            OutputStream out = new FileOutputStream("hello_world.wav");
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = in.read(buffer)) > 0) {
-                out.write(buffer, 0, length);
-            }
+                        InputStream inputStream
+                                = textToSpeech.synthesize(synthesizeOptions).execute();
+                        InputStream in = WaveUtils.reWriteWaveHeader(inputStream);
 
-            out.close();
-            in.close();
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-            
-            
-            
-            block();
+                        OutputStream out = new FileOutputStream("hello_world.wav");
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = in.read(buffer)) > 0) {
+                            out.write(buffer, 0, length);
+                        }
+
+                        out.close();
+                        in.close();
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                block();
             }
         });
     }
-    
+
 }
