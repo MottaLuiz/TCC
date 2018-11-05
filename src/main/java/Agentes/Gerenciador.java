dda190341b01c24f2c007918ee0499a4f8a8b595
Lidera Consultoria <Lidera Consultoria@LAPTOP-RC91CCDL>
2018-11-05 14:12:18 -0200

    Merge origin/master
    Conflicts:
    	src/main/bots/conhecimentodialogo/aiml/update.aiml
    	src/main/bots/conhecimentodialogo/aimlif/update.aiml.csv

--- src/main/java/Agentes/Gerenciador.java
+++ src/main/java/Agentes/Gerenciador.java
@@ -30,6 +30,7 @@ public class Gerenciador extends Agent {
     private PilhaDialogo pilha = new PilhaDialogo();
     private String resposta = new String();
     private FrameTarefa frame = new FrameTarefa();
+    String estado = new String();
 
     protected void setup() {
         System.out.println("Gerenciador de dialogo incializado");
@@ -37,6 +38,8 @@ public class Gerenciador extends Agent {
         resposta = null;
         pares = null;
         pilha.init();
+        estado = "EsperandoMSG";
+
         GerenciadorCasa gc = new GerenciadorCasa();
         try {
             gc.init();
@@ -44,141 +47,108 @@ public class Gerenciador extends Agent {
             Logger.getLogger(Gerenciador.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        FSMBehaviour compFSM = new FSMBehaviour(this) {
-
-        };
-// r e g i s t r am o s o p r im e i r o comportamento − X
         addBehaviour(new CyclicBehaviour(this) {
-            public void action() {
-                ACLMessage msgr = receive();
-                pares = null;
-                if (msgr != null) {
-                    try {
-                        pares = (Vector<Pares>) msgr.getContentObject();
-                        StringBuilder output = new StringBuilder();
-                        System.out.println("PARES RECEBIDOS");
 
-                    } catch (UnreadableException ex) {
-                        Logger.getLogger(Gerenciador.class
-                                .getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    //System.out.println(" - " + myAgent.getLocalName( )+"<- " + msgr.getContent());
-                }
-                // interrompe este comportamento ate que chegue uma nova mensagem
-                block();
-            }
-        });
-// registramos estado inicial − AnalisandoAtos
-        compFSM.registerFirstState(new OneShotBehaviour(this) {
             public void action() {
-                resposta = null;
-                if (pilha.vazia()) {
-                    if (pares != null) {
-                        if (pares.size() == 1) {
-                            pilha.setIntencaoatual(pares.elementAt(0).getIntencao());
-                            pilha.setArgsatual(pares.elementAt(0).getArgs());
-                        }
-                        if (pares.size() > 1) {
-                            for (int i = 0; i <= pares.size(); i++) {
-                                if (pares.elementAt(i).getIntencao().equals("Confirmar")) {
-                                    pilha.insere(pilha.getIntencaoatual(), pilha.getArgsatual());
-                                    pilha.setIntencaoatual(pares.elementAt(i).getIntencao());
-                                    pilha.setArgsatual(pares.elementAt(i).getArgs());
-                                } else {
-                                    pilha.insere(pares.elementAt(i).getIntencao(),
-                                            pares.elementAt(i).getArgs());
-                                }
+
+                switch (estado) {
+
+                    case "EsperandoMSG": {
+                        ACLMessage msgr = receive();
+                        pares = null;
+                        if (msgr != null) {
+                            try {
+                                pares = (Vector<Pares>) msgr.getContentObject();
+                                StringBuilder output = new StringBuilder();
+                                System.out.println("PARES RECEBIDOS");
+                                estado = "AtualizandoPilha";
+
+                            } catch (UnreadableException ex) {
+                                Logger.getLogger(Gerenciador.class
+                                        .getName()).log(Level.SEVERE, null, ex);
                             }
+
+                            //System.out.println(" - " + myAgent.getLocalName( )+"<- " + msgr.getContent());
+                        } else {
+                            estado = "EsperandoMSG";
                         }
                     }
-                } else {
+                    case "AtualizandoPilha": {
 
-                }
-            }
+                        resposta = null;
+                        if (pilha.vazia()) {
+                            if (pares != null) {
+                                if (pares.size() == 1) {
+                                    pilha.setIntencaoatual(pares.elementAt(0).getIntencao());
+                                    pilha.setArgsatual(pares.elementAt(0).getArgs());
+                                }
+                                if (pares.size() > 1) {
+                                    for (int i = 0; i <= pares.size(); i++) {
+                                        if (pares.elementAt(i).getIntencao().equals("Informarconfirmacao")) {
+                                            pilha.insere(pilha.getIntencaoatual(), pilha.getArgsatual());
+                                            pilha.setIntencaoatual(pares.elementAt(i).getIntencao());
+                                            pilha.setArgsatual(pares.elementAt(i).getArgs());
+                                        } else {
+                                            pilha.insere(pares.elementAt(i).getIntencao(),
+                                                    pares.elementAt(i).getArgs());
+                                        }
+                                    }
+                                }
+                                pares = null;
+                            }
+                        } else {//caso pilha nao vazia mais de um comando por frase
 
-            public int onEnd() {
-                if (pares != null) {
-                    return 1;
-                }
-                return 0;
-            }
-        },
-                 "AnalisandoAtos");
-// registramos outro estado − ProcessandoAtoAtual
-        compFSM.registerState(new OneShotBehaviour(this) {
-            int flag = 0;
+                        }
 
-            public void action() {
-                if (pilha.getIntencaoatual().equals("") || pilha.getArgsatual().equals("")) {
-                    resposta = TratadorErro.tratarerro(pilha.getIntencaoatual(), pilha.getArgsatual());
-                    flag = 2;
-                } else {
-                    tratarato(pilha.getIntencaoatual(), pilha.getArgsatual());
-                    flag = 1;
-                }
-            }
-
-            public int onEnd() {
-
-                return flag;
-            }
-
-        }, "ProcessandoAtoAtual");
-
-        // registramos outro estado − RespostaNula
-        compFSM.registerState(new OneShotBehaviour(this) {
-            int flag = 0;
-
-            public void action() {
-                if (pilha.vazia() && frame.getAcao() != null && frame.getDispositivo() != null && frame.getLocal() != null) {
-                    if (frame.getTarefa().equalsIgnoreCase("controlardispositivos")) {
-                        resposta = ExecutadorTarefa.executar(frame, gc);
-                        flag = 1;
+                        estado = "ProcessandoAtoAtual";
                     }
-                } else {
-                    pilha.remove();
-                    flag = 0;
+                    case "ProcessandoAtoAtual": {
+
+                        if (pilha.getArgsatual().equals("") || pilha.getIntencaoatual().equals("")) {
+
+                            resposta = TratadorErro.tratarerro(pilha.getIntencaoatual(), pilha.getArgsatual());
+                            estado = "ComunicaGLN";
+                        } else {
+                            tratarato(pilha.getIntencaoatual(), pilha.getArgsatual());
+                            estado = "PilhaVazia";
+                        }
+
+                    }
+                    case "PilhaVazia": {
+
+                        if (pilha.vazia() && frame.getAcao() != null && frame.getDispositivo() != null && frame.getLocal() != null) {
+                            if (frame.getTarefa().equalsIgnoreCase("controlardispositivos")) {
+                                resposta = ExecutadorTarefa.executar(frame, gc);
+                                estado = "ComunicaGLN";
+                            }
+                        } else {
+                            pilha.remove();
+                            estado = "AtualizaPilha";
+                        }
+
+                    }
+
+                    case "ComunicaGLN":{
+
+
+                            ACLMessage msge = new ACLMessage(INFORM);
+                            msge.setLanguage("Portugues");
+                            msge.addReceiver(new AID("GeradorLN", AID.ISLOCALNAME));
+
+                            try {
+                                msge.setContentObject(resposta);
+
+                            } catch (IOException ex) {
+                                Logger.getLogger(Gerenciador.class
+                                        .getName()).log(Level.SEVERE, null, ex);
+                            }
+                            send(msge);
+                            estado = "EsperandoMSG";
+                        }
+
                 }
-            }
 
-            public int onEnd() {
-
-                return flag;
-            }
-
-        }, "PilhaVazia");
-
-// registramos o ultimo estado − ComunicaGLN
-        compFSM.registerLastState(new OneShotBehaviour(this) {
-
-            public void action() {
-
-                ACLMessage msge = new ACLMessage(INFORM);
-                msge.setLanguage("Portugues");
-                msge.addReceiver(new AID("GeradorLN", AID.ISLOCALNAME));
-
-                try {
-                    msge.setContentObject(resposta);
-
-                } catch (IOException ex) {
-                    Logger.getLogger(Gerenciador.class
-                            .getName()).log(Level.SEVERE, null, ex);
-                }
-                send(msge);
-            }
-        }, "ComunicaGLN");
-        // definimos as transicoes
-        compFSM.registerTransition("AnalisandoAtos", "ProcessandoAtoAtual", 1);
-        compFSM.registerTransition("AnalisandoAtos", "AnalisandoAtos", 0);
-        compFSM.registerTransition("ProcessandoAtoAtual", "ComunicaGLN", 2);
-        compFSM.registerTransition("ProcessandoAtoAtual", "PilhaVazia", 1);
-        compFSM.registerTransition("PilhaVazia", "ProcessandoAtoAtual", 0);
-        compFSM.registerTransition("PilhaVazia", "ComunicaGLN", 1);
-        // acionamos o comportamento
-        addBehaviour(compFSM);
-
-    }
+    }});}
 
     private void tratarato(String intencaoatualaux, String argsatualaux) {
 
